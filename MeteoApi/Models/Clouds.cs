5091ef8e40a36d713fe9b73c5721898a778a4c58
Hubert Smiechowicz <hubert.smiechowicz@gmail.com>
2024-01-19 19:37:19 +0100

    upgrade image href

--- MeteoApi/Models/Clouds.cs
+++ MeteoApi/Models/Clouds.cs
@@ -28,27 +28,27 @@ namespace MeteoApi.Models
 
             if (rain > 0)
             {
-                return "src/assets/rain.png";
+                return "/src/assets/rain.png";
             }
             if (all >= 0 && all < 20 && rain == 0)
             {
-                return "src/assets/sunny.png";
+                return "/src/assets/sunny.png";
             }
             else if (all >= 20 && all < 40 && rain == 0)
             {
-                return "src/assets/littleClouds.png";
+                return "/src/assets/littleClouds.png";
             }
             else if (all >= 40 && all < 60 && rain == 0)
             {
-                return "src/assets/moreClouds.png";
+                return "/src/assets/moreClouds.png";
             }
             else if (all >= 60 && all < 80 && rain == 0)
             {
-                return "src/assets/bigClouds.png";
+                return "/src/assets/bigClouds.png";
             }
             else if (all >= 80 && all <= 100 && rain == 0)
             {
-                return "src/assets/hardClouds.png";
+                return "/src/assets/hardClouds.png";
             }
             else
             {
