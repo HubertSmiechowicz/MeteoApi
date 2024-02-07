5c459aac1edb0a8ef920284dd05224a07148476e
Hubert Śmiechowicz <hubert.smiechowicz@hotmail.com>
2024-02-07 21:46:03 +0100

    Changed images url

--- MeteoApi/Models/Air/Main.cs
+++ MeteoApi/Models/Air/Main.cs
@@ -18,11 +18,11 @@ namespace MeteoApi.Models.Air
         {
             return aqi switch
             {
-                1 => "/src/assets/laugh.svg",
-                2 => "/src/assets/smile.svg",
-                3 => "/src/assets/meh.svg",
-                4 => "/src/assets/frown.svg",
-                5 => "/src/assets/angry.svg",
+                1 => "https://www.dropbox.com/scl/fi/0uh9nao1pbqkc7gjmykvg/laugh.svg?rlkey=58l3urgziskccye3h78i26pn4&raw=1",
+                2 => "https://www.dropbox.com/scl/fi/u5sr30m7s7s1o17ojnx1m/smile.svg?rlkey=m2hme38ciwv3n70l9ei68u594&raw=1",
+                3 => "https://www.dropbox.com/scl/fi/xc8a0r0m2b0xrlg0wwxt4/meh.svg?rlkey=uyeq2ipa5453g78tgmiknd76m&raw=1",
+                4 => "https://www.dropbox.com/scl/fi/24d1ueqxr316y2l7ob39w/frown.svg?rlkey=y4l2k58gq935hq5j9mpockn6m&raw=1",
+                5 => "https://www.dropbox.com/scl/fi/49iy74nllg2y9ula8mnr2/angry.svg?rlkey=75czxeidqjarhm8zzyf2109xr&raw=1",
                 _ => "",
             };
         }
--- MeteoApi/Models/Clouds.cs
+++ MeteoApi/Models/Clouds.cs
@@ -28,27 +28,27 @@ namespace MeteoApi.Models
 
             if (rain > 0)
             {
-                return "/src/assets/rain.png";
+                return "https://www.dropbox.com/scl/fi/f11jnd351bk78zs7bfgw7/rain.png?rlkey=pjv771u4xfziik4shzdevxqm1&dl=1";
             }
             if (all >= 0 && all < 20 && rain == 0)
             {
-                return "/src/assets/sunny.png";
+                return "https://www.dropbox.com/scl/fi/0xl6tpppg3xwf8l7ul8cc/sunny.png?rlkey=hpptbk3d92du6kwlwdxrt7zwb&dl=1";
             }
             else if (all >= 20 && all < 40 && rain == 0)
             {
-                return "/src/assets/littleClouds.png";
+                return "https://www.dropbox.com/scl/fi/1sqw2taj03p6q962s0u0u/littleClouds.png?rlkey=fa5pcckr8gkrar93n7i2ai4xc&dl=1";
             }
             else if (all >= 40 && all < 60 && rain == 0)
             {
-                return "/src/assets/moreClouds.png";
+                return "https://www.dropbox.com/scl/fi/rx63musym67whea840ubv/moreClouds.png?rlkey=9moq91ifj1u2j728v7xu1unre&dl=1";
             }
             else if (all >= 60 && all < 80 && rain == 0)
             {
-                return "/src/assets/bigClouds.png";
+                return "https://www.dropbox.com/scl/fi/ypinqw62w4v7553dj55kf/bigClouds.png?rlkey=svwqs7h58ms4s8pf5ikq9g0bu&dl=1";
             }
             else if (all >= 80 && all <= 100 && rain == 0)
             {
-                return "/src/assets/hardClouds.png";
+                return "https://www.dropbox.com/scl/fi/nwsp4eif1l6mzju57h9kz/hardClouds.png?rlkey=yvpzpumoyz78ocf1t1u7c3dae&dl=1";
             }
             else
             {
